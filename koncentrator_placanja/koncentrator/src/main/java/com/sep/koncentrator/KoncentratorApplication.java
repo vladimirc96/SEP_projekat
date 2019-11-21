843d5137059547527cd857b5e0e12e35e0cab47f
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2019-11-22 00:16:45 +0100

    Testiranje

--- koncentrator_placanja/koncentrator/src/main/java/com/sep/koncentrator/KoncentratorApplication.java
+++ koncentrator_placanja/koncentrator/src/main/java/com/sep/koncentrator/KoncentratorApplication.java
@@ -2,9 +2,11 @@ package com.sep.koncentrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class KoncentratorApplication {
 
