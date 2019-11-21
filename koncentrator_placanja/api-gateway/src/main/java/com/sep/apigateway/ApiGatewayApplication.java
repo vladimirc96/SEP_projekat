1dbccc673930ee8fc805d22424d7cc5785291618
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2019-11-21 22:42:59 +0100

    Podignuti mikroservisi

--- koncentrator_placanja/api-gateway/src/main/java/com/sep/apigateway/ApiGatewayApplication.java
+++ koncentrator_placanja/api-gateway/src/main/java/com/sep/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.sep.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
