b701f9f39ee9c837b21395a2d3116a332eddf347
Vukasin <jovic.vukasin@gmail.com>
2019-12-23 20:58:47 +0100

    nzm

--- koncentrator_placanja/paypal-service/src/main/java/com/sep/paypalservice/config/PaypalConfig.java
+++ koncentrator_placanja/paypal-service/src/main/java/com/sep/paypalservice/config/PaypalConfig.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class PaypalConfig {
