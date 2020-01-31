3f60fc1edbb2f6fcf5c05dd1d5f3a5d4dd84ea30
Milan Lazic <lazicy@gmail.com>
2020-01-31 12:56:32 +0100

    New OrderStatus introduced

--- frontend/kp-app/src/app/subscription-plan/subscription-plan.component.ts
+++ frontend/kp-app/src/app/subscription-plan/subscription-plan.component.ts
@@ -1,68 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { PaypalService } from '../services/paypal.service';
-import { ActiveOrderService } from '../services/active-order.service';
-import { SellersService } from '../services/sellers.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { PaypalService } from "../services/paypal.service";
+import { ActiveOrderService } from "../services/active-order.service";
+import { SellersService } from "../services/sellers.service";
 
 @Component({
-  selector: 'app-subscription-plan',
-  templateUrl: './subscription-plan.component.html',
-  styleUrls: ['./subscription-plan.component.css']
+    selector: "app-subscription-plan",
+    templateUrl: "./subscription-plan.component.html",
+    styleUrls: ["./subscription-plan.component.css"]
 })
 export class SubscriptionPlanComponent implements OnInit {
+    id: any;
+    NC: string = "http://localhost:4201";
+    ppizbor: any;
+    PayPalPlans: any;
 
-  id: any;
-  NC: string = "http://localhost:4201";
-  ppizbor: any;
-  PayPalPlans: any;
+    imaPPPlanova: boolean = false;
+    seller: any = null;
+    activeOrder: any = null;
 
-  imaPPPlanova: boolean = false;
-  seller: any = null;
-	activeOrder: any = null;
+    constructor(
+        private activeOrderService: ActiveOrderService,
+        private sellerService: SellersService,
+        private paypalService: PaypalService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        this.route.params.subscribe((params: Params) => {
+            const param = +params["id"];
 
-  constructor(private activeOrderService: ActiveOrderService, private sellerService: SellersService, private paypalService: PaypalService, private route: ActivatedRoute, private router: Router) {
-    this.route.params.subscribe((params: Params) => {
-			const param = +params["id"];
+            if (!isNaN(param)) {
+                this.id = param;
+                this.getActiveOrder();
+            } else {
+                window.location.href = this.NC;
+            }
+        });
+    }
 
-			if (!isNaN(param)) {
-        this.id = param;
-        this.getActiveOrder();
-			} else {
-				window.location.href = this.NC;
-			}
-		});
-  }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    getActiveOrder() {
+        this.activeOrderService.getActiveOrder(this.id).subscribe(
+            success => {
+                this.activeOrder = success;
+                this.getPayPalSubscriptions(this.activeOrder.sellerId);
+            },
+            error => alert(error.error)
+        );
+    }
 
-  getActiveOrder(){
-		this.activeOrderService.getActiveOrder(this.id).subscribe(
-			(success) => {
-        this.activeOrder = success;
-        this.getPayPalSubscriptions(this.activeOrder.sellerId);
-			},
-			error => alert(error.error),
-		);
-  }
+    getPayPalSubscriptions(id) {
+        this.paypalService.getPaypalSubscriptionPlans(id).subscribe(
+            response => {
+                this.PayPalPlans = response;
+                if (
+                    this.PayPalPlans !== undefined &&
+                    this.PayPalPlans.length != 0
+                ) {
+                    this.ppizbor = this.PayPalPlans[0].id;
+                    this.imaPPPlanova = true;
+                }
+            },
+            error => {
+                alert(error.message);
+            }
+        );
+    }
 
-  getPayPalSubscriptions(id) {
-    this.paypalService.getPaypalSubscriptionPlans(id).subscribe(
-      (response) => {
-        this.PayPalPlans = response;
-        if(this.PayPalPlans !== undefined && this.PayPalPlans.length != 0) {
-          this.ppizbor = this.PayPalPlans[0].id;
-          this.imaPPPlanova = true;
-        }
-      },
-      (error) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  onPPSub() {
-    window.location.href = "https://localhost:4200/paypal/plan/subscribe/".concat(this.ppizbor).concat("/").concat(this.id);
-  }
-
+    onPPSub() {
+        window.location.href = "https://localhost:4200/paypal/plan/subscribe/"
+            .concat(this.ppizbor)
+            .concat("/")
+            .concat(this.id);
+	}
+	
+	onRegularSub() {
+		this.router.navigate(["/sellers/" + this.activeOrder.id]);
+	}
 }
